848894e19e5ceb0859157e935db73b9ec3978377
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-26 19:50:46 -0500

    built out my controllers for fans and posts

---
+++ app/controllers/fans_controller.rb
@@ -0,0 +1,61 @@
+class FansController < ApplicationController
+
+  #before_action :authenticate_user!, only: [:add_favorite, :remove_favorite]
+  # index
+  def index
+    @fans = fan.all
+  end
+
+  #show
+  def show
+    @fan = fan.find(params[:id])
+  end
+
+  # new
+  def new
+    @fan = Fan.new
+  end
+
+  # create
+  def create
+    @fan = Fan.create(fan_params)
+    redirect_to "/fans/#{@fan.id}"
+  end
+
+
+  # edit
+  def edit
+    @fan = Fan.find(params[:id])
+  end
+
+  # update
+  def update
+    @fan = Fan.find(params[:id])
+    @fan.update(fan_params)
+
+    redirect_to "/fans/#{@fan.id}"
+  end
+
+  # destroy
+  def destroy
+    @fan = Fan.find(params[:id])
+    @fan.destroy
+    redirect_to "/fans"
+  end
+
+  def add_favorite
+    @fan = Fan.find(params[:id])
+    @fan.favorites.create(user: current_user)
+    redirect :back
+  end
+
+  def remove_favorite
+    Favorite.where(user: current_user).destroy_all
+    redirect :back
+  end
+
+  private
+  def fan_params
+    params.require(:fan).permit(:name, :location, :age, :photo)
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  # index
+  def index
+    @posts = Post.all
+  end
+
+  # new
+  def new
+    @fan = Fan.find(params[:house_id])
+    @post = @fan.posts.new
+  end
+
+  # create
+  def create
+    @fan = Fan.find(params[:fan_id])
+    @post = @fan.posts.create(post_params)
+    redirect_to fan_post_path(@fan, @post)
+  end
+
+  #show
+  def show
+    @post = Post.find(params[:id])
+    @fan = Fan.find(@post.fan_id)
+  end
+
+  # edit
+  def edit
+    @post = Post.find(params[:id])
+    @fan = Fan.find(@post.fan_id)
+  end
+
+  # update
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    @fan = Fan.find(@post.fan_id)
+    redirect_to fan_post_path(@fan, @post)
+  end
+
+  # destroy
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    @fan = Fan.find(@post.fan_id)
+
+    redirect_to fan_path(@house)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:shoe_name, :shoe_photo, :fan_message)
+  end
+end
